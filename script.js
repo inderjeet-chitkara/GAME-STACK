c1647de33fd830f5ade488f03d47e3ee047c504e
inderjeet-chitkara <inderjeet3239.beai25@chitkara.edu.in>
2026-05-21 17:37:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// Select all buttons
+const buttons = document.querySelectorAll(".game-card button");
+
+// Load cart from localStorage
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+// Add event listener to each button
+buttons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        
+        const gameCard = button.parentElement;
+
+        const title = gameCard.querySelector("h3").innerText;
+        const price = gameCard.querySelector("p").innerText;
+
+        const image = gameCard.querySelector("img").src;
+
+const game = {
+    title: title,
+    price: price,
+    image: image
+};
+
+        // CHECK IF GAME ALREADY EXISTS
+const existingGame = cart.find(item =>
+    item.title === game.title
+);
+
+// IF ALREADY IN CART
+if(existingGame){
+
+    button.innerText = "Already in Cart";
+    button.disabled = true;
+
+    return;
+}
+
+// ADD GAME
+cart.push(game);
+
+// SAVE
+localStorage.setItem(
+    "cart",
+    JSON.stringify(cart)
+);
+
+// SUCCESS MESSAGE
+alert(title + " added to cart!");
+    });
+});
